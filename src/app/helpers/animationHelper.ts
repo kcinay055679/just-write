1a161c0d66436d2f5382470924b8c7f87e29a35f
Yanick <minder@puzzle.ch>
2024-11-27 20:19:14 +0100

    add animationHelper and icon

---
+++ src/app/helpers/animationHelper.ts
@@ -0,0 +1,17 @@
+export const paths:string[] = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink', 'brown', 'black', 'white'];
+
+export class AnimationHelper {
+    private imagePath:string;
+
+    constructor(imagePath: string) {
+        this.imagePath = imagePath;
+    }
+
+    public animateBackward():void {
+
+    }
+
+    public animateForward():void {
+
+    }
+}
