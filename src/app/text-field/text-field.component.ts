56a49b807aef71edb7998186b6c6994d7ab6ad2a
Yanick <minder@puzzle.ch>
2024-11-27 14:49:19 +0100

    add angular material

--- src/app/text-field/text-field.component.ts
+++ src/app/text-field/text-field.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-text-field',
-  imports: [],
+    imports: [
+        ReactiveFormsModule
+    ],
   templateUrl: './text-field.component.html',
   styleUrl: './text-field.component.scss'
 })
 export class TextFieldComponent {
+  text = new FormControl('');
 
 }
