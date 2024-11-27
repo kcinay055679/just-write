15fff2cda775b488bbea740ac131573f8c3e86fa
Yanick <minder@puzzle.ch>
2024-11-27 17:04:18 +0100

    add text change pipe chain

--- src/app/components/text-field/text-field.component.ts
+++ src/app/components/text-field/text-field.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {distinctUntilChanged, map, tap} from "rxjs";
 
 @Component({
   selector: 'app-text-field',
@@ -9,7 +10,15 @@ import {FormControl, ReactiveFormsModule} from "@angular/forms";
   templateUrl: './text-field.component.html',
   styleUrl: './text-field.component.scss'
 })
-export class TextFieldComponent {
+export class TextFieldComponent implements OnInit{
   text = new FormControl('');
 
+  ngOnInit(): void {
+    this.text.valueChanges.pipe(
+        map(e => e || ""),
+        distinctUntilChanged(),
+        tap(e=>console.log(e)
+        )
+    ).subscribe()
+  }
 }
